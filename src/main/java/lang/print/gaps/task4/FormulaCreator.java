75d3c357949b444aaf4da3cecbc607dc2d315d4e
SAmen <samenuatkhan@gmail.com>
2023-04-27 16:25:17 +0600

    done

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a, b;
+        a = 5;
+        b = 5;
+
+        double res = (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
     }
 }
